ccff1da6b11fb834b0c96a30a1aeec1d285db3b4
Rakyan Adhikara <rkyn.aa@gmail.com>
2023-11-29 08:41:58 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,12 +15,12 @@ const config = {
   url: 'https://rkynaa.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/album-docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'rkynaa', // Usually your GitHub org/user name.
-  projectName: 'rkynaa.github.io', // Usually your repo name.
+  projectName: 'album-docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
